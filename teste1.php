9ddf1d5d2488c0f53b81f3c4f931cbbed405d5cd
diogo <diogonbaa@gmail.com>
2017-01-08 16:14:24 -0200

    formulario php

--- teste1.php
+++ teste1.php
@@ -1,16 +1,32 @@
 <html>
 <head>
+	<meta charset="UTF-8">
+	<style type="text/css"> 
+		.center {
+    margin: auto;
+    width: 60%;
+    border: 3px solid #73AD21;
+    padding: 10px;
+
+	</style>
+
 </head>
 <body>
-<?php
-echo "estou aqiu ";
-$i=0;
-for($i=0;$i<=100;$i++){
-	
-	//echo "$i<br>";
-	echo "Denise pereira Denise pereira";
-}
-	
-?>
+	<form class="center">
+		Nome:<br>
+		<input type="text" name="Nome" value=""><br>
+		Idade:<br>
+		<input type="text" name="Idade" value=""><br>
+		sexo:<br>
+		<input type="text" name="Sexo" value=""><br>
+		Endereço:<br>
+		<input type="text" name="Endereço" value=""><br>
+		Numero:<br>
+		<input type="text" name="Numero" value=""><br>
+		Complemento:<br>
+		<input type="text" name="Complemento" value=""><br><br>
+		<input type="submit" value="Resultado">	
+	</form>
+
 </body>
 </html>
